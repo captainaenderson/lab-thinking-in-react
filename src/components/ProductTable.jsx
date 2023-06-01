c1de45b451f13542abe86c6e8b90826108af1f9d
Ann-Kathrin Wohlrab <annkathrin.wohlrab@gmail.com>
2023-06-01 09:36:40 +0200

    Iteration 1 + 2

---
+++ src/components/ProductTable.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function ProductTable() {
+  return (
+    <div><input type="checkbox" name="" id="" />Only show products in Stock</div>
+  )
+}
+
+export default ProductTable
---
+++ src/components/SearchBar.jsx
@@ -0,0 +1,14 @@
+function SearchBar() {
+  return (
+    <>
+      <div>
+        <h2>Search</h2>
+      </div>
+      <div>
+        <input type="text" />
+      </div>
+    </>
+  );
+}
+
+export default SearchBar;
